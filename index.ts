c5a7163c5e8ef62d62699241bc2d52f2cefecbf5
Blue Bread <redbread1029@gmail.com>
2020-07-29 21:24:24 -0300

    soma de dois numeros

---
+++ index.ts
@@ -0,0 +1,5 @@
+let um:string = prompt("Primeiro nome")
+let dois:string = prompt("Sobrenome")
+let idade:number = Number(prompt("idade"))
+let resultado:string = "Olá " + um + " " + dois +", " + idade + " anos"
+console.log(resultado)
---
+++ js/index.js
@@ -0,0 +1,5 @@
+var um = prompt("Primeiro nome");
+var dois = prompt("Sobrenome");
+var idade = Number(prompt("idade"));
+var resultado = "Olá " + um + " " + dois + ", " + idade + " anos";
+console.log(resultado);
